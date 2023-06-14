85b8806d91a3bb885abf83bfe4855eda3ab6623b
Hide <hshidara@ucdavis.edu>
2023-06-14 14:39:10 -0700

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <main>
+        hello world
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  namespace :api do
+  end
+  # all other routes will be load our React application
+  # this route definition matches:
+  # - *path: all paths not matched by one of the routes defined above
+  # - constraints:
+  #   - !req.xhr?: it's not a XHR (fetch) request
+  #   - req.format.html?: it's a request for a HTML document
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+end
