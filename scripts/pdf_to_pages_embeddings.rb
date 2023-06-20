b0d6c06fd91c7171f3f3c8dbbcdd31f99b75e700
Hide <hshidara@ucdavis.edu>
2023-06-20 02:29:44 -0700

    adding ruby script to generate pdf and embeddings csv

--- scripts/pdf_to_pages_embeddings.rb
+++ scripts/pdf_to_pages_embeddings.rb
@@ -1,60 +1,83 @@
-require 'dotenv'
+require 'dotenv/load'
+require 'daru'
+require 'csv'
 require 'openai'
 require 'pdf-reader'
-require 'csv'
 require 'transformers'
-require 'fileutils'
+require 'tokenizers'
+require 'optparse'
 
 Dotenv.load('.env')
 
-OpenAI.configure do |config|
-  config.access_token = ENV["OPENAI_API_KEY"]
-end
+OPENAI_API_KEY = ENV['OPENAI_KEY']
 
-COMPLETIONS_MODEL = "text-davinci-003"
-MODEL_NAME = "curie"
+COMPLETIONS_MODEL = 'text-davinci-003'
+MODEL_NAME = 'curie'
 DOC_EMBEDDINGS_MODEL = "text-search-#{MODEL_NAME}-doc-001"
 
-tokenizer = Transformers::BertTokenizer.from_pretrained("bert-base-uncased")
-
 def count_tokens(text)
-  tokens = tokenizer.tokenize(text)
-  tokens.length
+  tokenizer = Tokenizers.from_pretrained("gpt2")
+  tokenizer.encode(text).tokens.length
 end
 
 def extract_pages(page_text, index)
-  return [] if page_text.empty?
-
-  content = page_text.split.join(" ")
-  puts "page text: #{content}"
-  outputs = [["Page #{index}", content, count_tokens(content) + 4]]
-
-  outputs
+  content = page_text.split.join(' ')
+  # puts "page text: #{content}"
+  ["Page #{index.to_s}", content.to_s, (count_tokens(content) + 4).to_s]
 end
 
-filename = ARGV[1]
+options = {}
+OptionParser.new do |opts|
+  opts.banner = 'Usage: ruby pdf_to_pages_embeddings.rb --pdf FILENAME'
 
-reader = PDF::Reader.new(File.open(filename, "rb"))
+  opts.on('--pdf FILENAME', 'Name of PDF') do |filename|
+    options[:filename] = filename
+  end
+end.parse!
+
+reader = PDF::Reader.new(options[:filename])
+
 res = []
 i = 1
-
 reader.pages.each do |page|
-  res += extract_pages(page.text, i)
+  res << extract_pages(page.text, i)
   i += 1
 end
 
-df = Pandas::DataFrame.new(res, columns: ["title", "content", "tokens"])
-df = df[df.tokens < 2046]
-df.reset_index!.drop("index", axis: 1)
-df.head()
+titles = []
+contents = []
+tokens = []
 
-df.to_csv("#{filename}.pages.csv", index: false)
+res.each do |tuple|
+  titles << tuple[0].to_s
+  contents << tuple[1].to_s
+  tokens << tuple[2].to_i
+end
+
+data = {
+  "title": titles,
+  "content": contents,
+  "tokens": tokens
+}
+
+df = Daru::DataFrame.new(data)
+df = df.filter_rows { |row| !row[:title].nil? && !row[:content].nil? && !row[:tokens].nil? }
+df = df.reindex(Daru::Index.new(0...df.nrows))
+
+CSV.open('book.pdf.pages.csv', 'w') do |csv|
+  csv << df.vectors.to_a # Write the column names as the first row
+  df.each_row { |row| csv << row.to_a }
+end
 
 def get_embedding(text, model)
-  OpenAI::Embedding.create(
-    model: model,
-    inputs: text
-  ).to_h["embeddings"][0]
+  client = OpenAI::Client.new(access_token: OPENAI_API_KEY)
+  result = client.embeddings(
+    parameters: {
+      model: model,
+      input: text
+    }
+  )
+  result['data'][0]['embedding']
 end
 
 def get_doc_embedding(text)
@@ -62,18 +85,18 @@ def get_doc_embedding(text)
 end
 
 def compute_doc_embeddings(df)
-  doc_embeddings = {}
-  df.iterrows.each do |idx, r|
-    doc_embeddings[idx] = get_doc_embedding(r.content)
+  embeddings = {}
+  df.each_row_with_index do |row, idx|
+    embeddings[idx] = get_doc_embedding(row[:content])
   end
-  doc_embeddings
+  embeddings
 end
 
 doc_embeddings = compute_doc_embeddings(df)
 
-CSV.open("#{filename}.embeddings.csv", "w") do |f|
-  f << ["title"] + (0..4095).to_a
-  doc_embeddings.each do |i, embedding|
-    f << ["Page #{i + 1}"] + embedding
+CSV.open('book.pdf.embeddings.csv', 'w') do |csv|
+  csv << ['title'] + (0..4095).to_a
+  doc_embeddings.each do |idx, embedding|
+    csv << ["Page #{idx + 1}"] + embedding
   end
 end
